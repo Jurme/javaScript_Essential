5cebe8e6aa4d1084279a5ee22beef04c6b1af161
Jurme <arojurmetenzin@gmail.com>
2026-05-07 05:10:30 -0400

    change

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,9 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
+        
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -43,6 +45,10 @@ function editbook(index) {
             document.getElementById('authorName').value = '';
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
+ }
+ function deletebook(index){
+books.splice(index, 1);
+showbooks();
  }
 
 
